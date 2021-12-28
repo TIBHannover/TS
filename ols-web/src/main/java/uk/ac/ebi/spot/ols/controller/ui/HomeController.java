110c53cc8e2eb894d4d810f101b02122b7c72dd0
Erhun Giray Tuncay <giray.tuncay@tib.eu>
2021-12-28 17:39:05 +0100

    implemented optional faceted filtering scenario with a checkbox for #47

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/HomeController.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/HomeController.java
@@ -305,7 +305,7 @@ public class HomeController {
         }
         
         model.addAttribute("searchOptions", searchOptions);
-//        model.addAttribute("availableSchemaValues",getClassificationsForSchemas(searchOptions.getSchemas()));
+        model.addAttribute("availableSchemaValues",getClassificationsForSchemas(searchOptions.getSchemas()));
         model.addAttribute("collectionValues",getClassificationsForSchemas(new ArrayList<String>(Arrays.asList("collection"))));
         customisationProperties.setCustomisationModelAttributes(model);
         return "search";
