bb68609a34f7537aab04402c196ef8f47434751d
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-04 11:33:59 +0100

    changed

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/entities/HttpServletRequestInfo.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/entities/HttpServletRequestInfo.java
@@ -1,32 +1,44 @@
 package uk.ac.ebi.spot.ols.entities;
 
+import java.util.Set;
+
 public class HttpServletRequestInfo {
-    private String address;
-    private String parameters;
-    private String keyValueParameters;
+    private String url;
+    private Set<RestCallParameter> pathVariables;
+    private Set<RestCallParameter> queryParameters;
 
     public HttpServletRequestInfo() {
     }
 
-    public HttpServletRequestInfo(String address, String parameters, String keyValueParameters) {
-        this.address = address;
-        this.parameters = parameters;
-        this.keyValueParameters = keyValueParameters;
+    public HttpServletRequestInfo(String url,
+                                  Set<RestCallParameter> pathVariables,
+                                  Set<RestCallParameter> queryParameters) {
+        this.url = url;
+        this.pathVariables = pathVariables;
+        this.queryParameters = queryParameters;
     }
 
-    public String getAddress() {
-        return address;
+    public String getUrl() {
+        return url;
     }
 
-    public String getParameters() {
-        return parameters;
+    public void setUrl(String url) {
+        this.url = url;
     }
 
-    public String getKeyValueParameters() {
-        return keyValueParameters;
+    public Set<RestCallParameter> getPathVariables() {
+        return pathVariables;
     }
 
-    public void setKeyValueParameters(String keyValueParameters) {
-        this.keyValueParameters = keyValueParameters;
+    public void setPathVariables(Set<RestCallParameter> pathVariables) {
+        this.pathVariables = pathVariables;
+    }
+
+    public Set<RestCallParameter> getQueryParameters() {
+        return queryParameters;
+    }
+
+    public void setQueryParameters(Set<RestCallParameter> queryParameters) {
+        this.queryParameters = queryParameters;
     }
 }
