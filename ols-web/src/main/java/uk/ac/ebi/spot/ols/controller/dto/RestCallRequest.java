20e5f917569458dbae1417915b694e3a2cc4f5f0
Natalia Andrianova <natalia.andrianova@tib.eu>
2022-02-04 11:37:18 +0100

    added parameter type and name

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/dto/RestCallRequest.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/dto/RestCallRequest.java
@@ -1,27 +1,46 @@
 package uk.ac.ebi.spot.ols.controller.dto;
 
+import uk.ac.ebi.spot.ols.entities.RestCallParameter;
+import uk.ac.ebi.spot.ols.entities.RestCallParameterType;
+
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class RestCallRequest {
-    private String address;
+    private String url;
+    private Optional<RestCallParameterType> type;
+    private Optional<String> parameterName;
     private LocalDateTime dateTimeFrom;
     private LocalDateTime dateTimeTo;
 
     public RestCallRequest() {
     }
 
-    public RestCallRequest(String address, LocalDateTime dateTimeFrom, LocalDateTime dateTimeTo) {
-        this.address = address;
+    public RestCallRequest(String url, LocalDateTime dateTimeFrom, LocalDateTime dateTimeTo) {
+        this.url = url;
         this.dateTimeFrom = dateTimeFrom;
         this.dateTimeTo = dateTimeTo;
     }
 
-    public String getAddress() {
-        return address;
+    public RestCallRequest(String url,
+                           Optional<RestCallParameterType> type,
+                           Optional<String> parameterName,
+                           LocalDateTime dateTimeFrom,
+                           LocalDateTime dateTimeTo) {
+        this.url = url;
+        this.type = type;
+        this.parameterName = parameterName;
+        this.dateTimeFrom = dateTimeFrom;
+        this.dateTimeTo = dateTimeTo;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public LocalDateTime getDateTimeFrom() {
@@ -38,5 +57,33 @@ public class RestCallRequest {
 
     public void setDateTimeTo(LocalDateTime dateTimeTo) {
         this.dateTimeTo = dateTimeTo;
+    }
+
+    public Optional<RestCallParameterType> getType() {
+        return type;
+    }
+
+    public void setType(Optional<RestCallParameterType> type) {
+        this.type = type;
+    }
+
+    public Optional<String> getParameterName() {
+        return parameterName;
+    }
+
+    public void setParameterName(Optional<String> parameterName) {
+        this.parameterName = parameterName;
+    }
+
+    public Predicate<RestCallParameter> getParameterNamePredicate() {
+        return parameterName.isPresent()
+            ? parameter -> parameterName.get().equalsIgnoreCase(parameter.getName())
+            : parameter -> true;
+    }
+
+    public Predicate<RestCallParameter> getParameterTypePredicate() {
+        return type.isPresent()
+            ? type.get().getRestCallParameterPredicate()
+            : parameter -> true;
     }
 }
