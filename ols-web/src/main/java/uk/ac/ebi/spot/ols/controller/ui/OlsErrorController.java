925d6bd26c374430194c0538e248e176c3033ca0
Erhun Giray Tuncay <giray.tuncay@tib.eu>
2022-03-08 11:01:53 +0100

    resolved ols error controller ui class

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/OlsErrorController.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/OlsErrorController.java
@@ -1,15 +1,12 @@
 
 package uk.ac.ebi.spot.ols.controller.ui;
 
-<<<<<<< HEAD
 import java.util.Date;
 import java.util.TimeZone;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-=======
->>>>>>> 6b26b5e43ada0ebc714898f7a81a1620b94f0802
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
@@ -23,7 +20,6 @@ public class OlsErrorController implements ErrorController  {
     private CustomisationProperties customisationProperties;
 
     @RequestMapping("/error")
-<<<<<<< HEAD
     public String handleError(HttpServletRequest request,Model model) {
     	
     	Date now = new Date();
@@ -31,9 +27,6 @@ public class OlsErrorController implements ErrorController  {
     	model.addAttribute("timestamp",now); 
     	model.addAttribute("message",request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
     	model.addAttribute("status",request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
-=======
-    public String handleError(Model model) {
->>>>>>> 6b26b5e43ada0ebc714898f7a81a1620b94f0802
 
         customisationProperties.setCustomisationModelAttributes(model);
 
@@ -42,11 +35,7 @@ public class OlsErrorController implements ErrorController  {
 
     @Override
     public String getErrorPath() {
-<<<<<<< HEAD
         return "/error";
-=======
-        return null;
->>>>>>> 6b26b5e43ada0ebc714898f7a81a1620b94f0802
     }
 }
 
