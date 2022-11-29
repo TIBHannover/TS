57969394a92b53787d12dc6b2edf8dc7e1a4763f
Erhun Giray Tuncay <giray.tuncay@tib.eu>
2022-11-29 17:35:12 +0100

    added ontology extension filter parameter to the UI Controller of an ontology for #85

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/OntologyControllerUI.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/OntologyControllerUI.java
@@ -122,7 +122,8 @@ public class OntologyControllerUI {
 
     @RequestMapping(path = "/{onto}", method = RequestMethod.GET)
     String getTerm(
-            @PathVariable("onto") String ontologyId,
+            @PathVariable("onto") String ontologyId, 
+            @RequestParam(value = "filter", required = true, defaultValue = "MAPPING_ONTOLOGIES") RepoFilterEnum filter,
             Model model) throws ResourceNotFoundException {
 
         ontologyId = ontologyId.toLowerCase();
@@ -144,9 +145,9 @@ public class OntologyControllerUI {
             model.addAttribute("ontologyDocument", document); 
             if(document.getConfig().getRepoUrl() != null)
                 if(document.getConfig().getRepoUrl().startsWith("http") && document.getConfig().getRepoUrl().contains("github")) {
-                	model.addAttribute("releaseUrls", repoMetadataService.releasesGithubREST(document.getConfig().getRepoUrl(),RepoFilterEnum.MAPPING_ONTOLOGIES, ontologyId));	
+                	model.addAttribute("releaseUrls", repoMetadataService.releasesGithubREST(document.getConfig().getRepoUrl(),filter, ontologyId));	
                 } else if (document.getConfig().getRepoUrl().startsWith("http"))
-                	model.addAttribute("releaseUrls", repoMetadataService.releasesGitlabREST(document.getConfig().getRepoUrl(), RepoFilterEnum.MAPPING_ONTOLOGIES, ontologyId));
+                	model.addAttribute("releaseUrls", repoMetadataService.releasesGitlabREST(document.getConfig().getRepoUrl(), filter, ontologyId));
                 
 
             customisationProperties.setCustomisationModelAttributes(model);
